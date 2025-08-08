f49d8b9fc82a9924c455e47479dfdd12569d078e
Vanshika <vanshikaraje010@gmail.com>
2025-08-08 18:16:17 +0530

    Update axios baseURL to deployed backend URL

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://assignment-backend-sand.vercel.app/api",
 });
 
 export default API;
-
